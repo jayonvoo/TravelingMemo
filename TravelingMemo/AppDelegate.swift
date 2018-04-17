717d393097ac826927da50b2ebb61d7d9b4e9658
John <kiyoto_zy@hotmail.com>
2018-04-17 13:27:50 +0800

    update 1327

--- TravelingMemo/AppDelegate.swift
+++ TravelingMemo/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var apiKey = "AIzaSyDZLNijX8vgAlsy_K2hgDeLymkLUvptlDk"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
