a17c7a42edfa3254205aa6a1aca4cd2925b756c4
John <kiyoto_zy@hotmail.com>
2018-04-18 10:20:21 +0800

    update 1020

--- TravelingMemo/ViewController.swift
+++ TravelingMemo/ViewController.swift
@@ -14,9 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var mapView: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let camera = GMSCameraPosition.camera(withLatitude: 24.175750, longitude: 120.647807, zoom: 10)
+        let camera = GMSCameraPosition.camera(withLatitude: 24.175750, longitude: 120.647807, zoom: 15)
         mapView.camera = camera
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.mapType = .satellite
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 24.175750, longitude: 120.647807)
+        marker.map = mapView
+        marker.title = "住家"
+        marker.snippet = "台灣"
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,5 +30,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
