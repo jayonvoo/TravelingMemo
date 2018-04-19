a468dabcd70ac748c366520824cb330d4cdddb5c
John <kiyoto_zy@hotmail.com>
2018-04-19 17:44:45 +0800

    update 1742

---
+++ ParoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ParoTableViewCell.swift
+//  TravelingMemo
+//
+//  Created by 創意遊玩 on 2018/4/18.
+//  Copyright © 2018年 創意遊玩. All rights reserved.
+//
+
+import UIKit
+
+class ParoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
