cbbe45b1c6b159f93dbd41c3c6ef036c8a521b6f
John <kiyoto_zy@hotmail.com>
2018-04-20 15:44:58 +0800

    update 1544

---
+++ TravelingMemo/MapViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MapViewController.swift
+//  TravelingMemo
+//
+//  Created by 創意遊玩 on 2018/4/20.
+//  Copyright © 2018年 創意遊玩. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController, GMSMapViewDelegate {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 24.175750, longitude: 120.647807, zoom: 15)
+        mapView.camera = camera
+        mapView.mapType = .satellite
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 24.175750, longitude: 120.647807)
+        marker.map = mapView
+        marker.title = "住家"
+        marker.snippet = "台灣"
+        
+        mapView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        performSegue(withIdentifier: "showPano", sender: nil)
+        //print("clicked")
+    }
+    /*
+    //隱藏頂上狀態列
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }*/
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
--- TravelingMemo/ViewController.swift
+++ TravelingMemo/ViewController.swift
@@ -17,37 +17,20 @@ class ViewController: UIViewController, GMSMapViewDelegate, UICollectionViewData
     var locationImage = ["lake", "rainbow", "river"]
     var locationDescription = ["靜謐的都市綠地空間，以自然步道和充滿魚群、烏龜和鳥類的大型生態池聞名。","鄰近嶺東科技大學，是條色彩繽紛、充滿童趣彩繪的巷道。","柳川水岸目前是親水兼具防洪效果的河岸分三區栽植楓香、黃花風鈴木、欒樹等植栽~打造出全年四季不同的河岸風貌。"]
     
-    @IBOutlet weak var mapView: GMSMapView!
+    
+    @IBAction func btn2mapAction(_ sender: AnyObject) {
+        
+        performSegue(withIdentifier: "btn2mapLink", sender: self)
+        
+    }
     override func viewDidLoad() {
-        /*
-         super.viewDidLoad()
-         let camera = GMSCameraPosition.camera(withLatitude: 24.175750, longitude: 120.647807, zoom: 15)
-         mapView.camera = camera
-         mapView.mapType = .satellite
-         
-         let marker = GMSMarker()
-         marker.position = CLLocationCoordinate2D(latitude: 24.175750, longitude: 120.647807)
-         marker.map = mapView
-         marker.title = "住家"
-         marker.snippet = "台灣"
-         
-         mapView.delegate = self
-         */
+        
+        super.viewDidLoad()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    //隱藏頂上狀態列
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
-        performSegue(withIdentifier: "showPano", sender: nil)
-        //print("clicked")
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -65,4 +48,9 @@ class ViewController: UIViewController, GMSMapViewDelegate, UICollectionViewData
         
         return cell
     }
+    /*
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        self.navigationController?.setToolbarHidden(true, animated: animated)
+    }*/
 }
